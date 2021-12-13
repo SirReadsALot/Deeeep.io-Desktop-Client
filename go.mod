0b13c6768c08b0e59343328b9af6c97a14160314
SirReadsALot <72465167+SirReadsALot@users.noreply.github.com>
2021-12-13 12:34:36 +0400

    implemented discord rpc

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/SirReadsALot/deep_desktop
 
 go 1.16
 
-require github.com/zserge/lorca v0.1.10
+require (
+	github.com/hugolgst/rich-go v0.0.0-20210925091458-d59fb695d9c0
+	github.com/zserge/lorca v0.1.10
+)
 
 replace github.com/zserge/lorca => github.com/CarrotzRule123/lorca v0.1.11
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"net/url"
-	"time"
-	"os"
-	"fmt"
-	"path"
 	"encoding/json"
+	"fmt"
+	"net/url"
+	"os"
+	"path"
+	"time"
 
+	"github.com/hugolgst/rich-go/client"
 	"github.com/zserge/lorca"
 )
 
@@ -16,17 +17,24 @@ type Config struct {
 }
 
 func main() {
+	/*
+		if _, err := os.Stat("/plugins"); os.IsNotExist(err) {
+			var err = os.Mkdir("plugins", 0755)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	*/
 	cwd, _ := os.Getwd()
-	docpath := path.Join(cwd, "docassets")
+	docpath := path.Join(cwd, "plugins/docassets")
 	// swappath := path.Join(cwd, "swapper")
 	flags := fmt.Sprintf("--load-extension=%v", docpath)
-
 	var config Config
 	data, err := os.ReadFile("config.json")
 	if err != nil {
-		config = Config {false}
+		config = Config{false}
 		data, _ := json.Marshal(config)
-    	os.WriteFile("config.json", data, 0644)
+		os.WriteFile("config.json", data, 0644)
 	} else {
 		json.Unmarshal(data, &config)
 	}
@@ -37,28 +45,44 @@ func main() {
 		<style>*{padding: 0; margin: 0; overflow: hidden}</style>
 		<img id="img" src="https://tinyurl.com/m5a4d3vw"></img>
 	`)+query(config), "", 887, 586, flags)
-	time.Sleep(0 * time.Second)
+	time.Sleep(4 * time.Second)
+
+	err = client.Login("817817065862725682")
+	if err != nil {
+		panic(err)
+	}
+	now := time.Now()
+	err = client.SetActivity(client.Activity{
+		Details:    "Playing Unknown gamemode",
+		LargeImage: "deeplarge_2",
+		LargeText:  "Playing Deeeep.io Desktop Client",
+		SmallImage: "ffa",
+		SmallText:  "Playing Unknown Gamemode",
+		Timestamps: &client.Timestamps{
+			Start: &now,
+		},
+	})
 
 	ui.Bind("setdocassets", func() {
 		config.Docassets = !config.Docassets
-		ui.Load(`https://deeeep.io`+query(config))
+		ui.Load(`https://deeeep.io` + query(config))
 		menu(ui)
 	})
 
 	ui.Bind("reload", func() {
-		ui.Load(`https://deeeep.io`+query(config))
+		ui.Load(`https://deeeep.io` + query(config))
 		menu(ui)
 	})
-	
-	ui.Load(`https://deeeep.io`+query(config))
-	ui.SetBounds(lorca.Bounds {0, 0, 1200, 1000, "maximized"})
+
+	ui.Load(`https://deeeep.io` + query(config))
+	ui.SetBounds(lorca.Bounds{0, 0, 1200, 1000, "maximized"})
 	menu(ui)
 
 	defer func() {
 		ui.Close()
 		if cfcpy != config {
 			data, _ := json.Marshal(config)
-    		os.WriteFile("config.json", data, 0644)
+			os.WriteFile("config.json", data, 0644)
 		}
 	}()
 	<-ui.Done()
@@ -68,6 +92,11 @@ func query(config Config) string {
 	return fmt.Sprintf("?docassets=%v", config.Docassets)
 }
 
+/* default
+sirreads' pc:
+GOARCH=amd64
+GOOS=windows
+*/
 func menu(ui lorca.UI) {
 	ui.Eval(`
 	window.onload = () => {
@@ -77,7 +106,14 @@ func menu(ui lorca.UI) {
 		div.style.zIndex = "100"
 		div.style.width = "100%"
 		div.innerHTML = '<nav id="menu" class="navbar navbar-expand navbar-dark bg-dark">\
-		<div class="collapse navbar-collapse">\
+			<!-- <ul class="navbar-nav">
+				<li class"nav-item"><a class="nav-link pull-left" onclick="open_navbar()">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="navbar-stuff">\
+						<span class="navbar-toggler-icon"></span>\
+					</button>\
+				</a></li>\
+			</ul>\ -->
+		<div id="navbar-stuff">
 			<ul class="navbar-nav mr-auto">\
 				<li class="nav-item"><a class="nav-link" id="docassets">\
 					<img style="width: 30px; height: 30px;" src="https://lh3.googleusercontent.com/JwLoyKugJQ-GNq-hGK94I23EJETk2_2Wi3UrLlShwBa1UTgiWFQQ4uvmKfxi7pacYwBbjg1IxOeus7Tlv8h5-Lui_5Q=w128-h128-e365-rj-sc0x00ffffff">\
@@ -85,13 +121,14 @@ func menu(ui lorca.UI) {
 						style="font-size: 10px; margin-top: -10px">ON</div>\
 				</a></li>\
 			</ul>\
-			<ul class="navbar-nav">\
-				<li class="nav-item"><a class="nav-link pull-right" onclick="dark()">\
-					<svg style="width: 20px; height: 20px;" viewBox="0 0 24 24" fill="currentColor">\
-						<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />\
-					</svg>\
-				</a></li>\
-			</ul>\
+				<ul class="navbar-nav">\
+					<li class="nav-item"><a class="nav-link pull-right" onclick="dark()">\
+						<svg style="width: 20px; height: 20px;" viewBox="0 0 24 24" fill="currentColor">\
+							<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />\
+						</svg>\
+					</a></li>\
+				</ul>\
+			</div>\
 		</div>\
 		</nav>'
 		document.body.insertBefore(div, app)
@@ -127,6 +164,10 @@ func menu(ui lorca.UI) {
 			menu.className = "navbar navbar-expand navbar-dark bg-dark"
 		} else {
 			menu.className = "navbar navbar-expand navbar-light bg-light"
-		}
+	}
+	
+	function open_navbar() {
+		console.log("DDDDDDDDDDDDDDDD")
+	}
 	}`)
 }
