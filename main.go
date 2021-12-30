d4e72ff1bc0f65db894ace439c7bd4187f0cde88
SirReadsALot <72465167+SirReadsALot@users.noreply.github.com>
2021-12-30 18:01:59 +0400

    Fixed client crashing when rpc is not detected

--- main.go
+++ main.go
@@ -47,12 +47,13 @@ func main() {
 		<!-- <img id="img" src="https://tinyurl.com/m5a4d3vw"></img> -->
 		<img id="img" src="https://sralcodeproj.netlify.app/assets/myhailot.png"></img>
 		<h3 id="loading">Loading...</h3>
-	`)+query(config), "", 1120, 740, flags) // previous: 887x586
+	`)+query(config), "", 1120, 740, flags, "--disable-dinosaur-easter-egg") // previous: 887x586
 	time.Sleep(5 * time.Second)
 
 	err = client.Login("817817065862725682")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		// panic(err) //this apparently crashes the entire client if it doesn't find disc
 	}
 	now := time.Now()
 	err = client.SetActivity(client.Activity{
@@ -68,16 +69,16 @@ func main() {
 
 	ui.Bind("setdocassets", func() {
 		config.Docassets = !config.Docassets
-		ui.Load(`https://deeeep.io` + query(config))
+		ui.Load(`https://beta.deeeep.io` + query(config))
 		menu(ui)
 	})
 
 	ui.Bind("reload", func() {
-		ui.Load(`https://deeeep.io` + query(config))
+		ui.Load(`https://beta.deeeep.io` + query(config))
 		menu(ui)
 	})
 
-	ui.Load(`https://deeeep.io` + query(config))
+	ui.Load(`https://beta.deeeep.io` + query(config))
 	ui.SetBounds(lorca.Bounds{0, 0, 1200, 1000, "maximized"})
 	menu(ui)
 
@@ -104,29 +105,13 @@ func menu(ui lorca.UI) {
 	ui.Eval(`
 	window.onload = () => {
 		const app = document.getElementById("app")
-		const div = document.createElement('div')
-		div.style.position = "absolute"
-		div.style.zIndex = "100"
-		div.style.width = "100%"
-		div.innerHTML = '<nav id="menu" class="navbar navbar-expand navbar-dark bg-dark">\
-		<div class="collapse navbar-collapse">\
-			<ul class="navbar-nav mr-auto">\
-				<li class="nav-item"><a class="nav-link" id="docassets">\
-					<img style="width: 30px; height: 30px;" src="https://lh3.googleusercontent.com/JwLoyKugJQ-GNq-hGK94I23EJETk2_2Wi3UrLlShwBa1UTgiWFQQ4uvmKfxi7pacYwBbjg1IxOeus7Tlv8h5-Lui_5Q=w128-h128-e365-rj-sc0x00ffffff">\
-				    <div id="doc-btn" class="position-absolute bg-success text-white px-1 rounded"\
-						style="font-size: 10px; margin-top: -10px">ON</div>\
-				</a></li>\
-			</ul>\
-			<ul class="navbar-nav">\
-				<li class="nav-item"><a class="nav-link pull-right" onclick="dark()">\
-					<svg style="width: 20px; height: 20px;" viewBox="0 0 24 24" fill="currentColor">\
-						<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />\
-					</svg>\
-				</a></li>\
-			</ul>\
-		</div>\
-		</nav>'
-		document.body.insertBefore(div, app)
+		//const div = document.createElement('div')
+		//div.style.position = "absolute"
+		//div.style.zIndex = "100"
+		//div.style.width = "100%"
+		// div.innerHTML = ''
+		document.getElementByClassName("el-row justify-center").innerHTML += "<div class="el-col el-col-12 el-col-sm-8 is-guttered text-center"><button class="el-button el-button--default btn nice-button green has-icon square"></button></div>"
+		// document.body.insertBefore(div, app)
 
 		const url = new URL(window.location.href);
 		const enabled = url.searchParams.get("docassets")
