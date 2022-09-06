f58193da170ea7c029090254e667a5cb2bc90e33
SirReadsALot <72465167+SirReadsALot@users.noreply.github.com>
2022-09-06 19:24:44 +0400

    Add files via upload

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/SirReadsALot/deep_desktop/src"
+	core "github.com/SirReadsALot/deep_desktop/src"
 	"github.com/zserge/lorca"
 )
 
@@ -23,13 +23,27 @@ func main() {
 	plugins := core.NewPluginManager()
 	plugins.AddPlugin(core.EXTENSION, "Docassets", "docassets", true)
 	plugins.AddPlugin(core.EXTENSION, "Swapper", "swapper", true)
-	plugins.AddPlugin(core.SCRIPT, "Discord RPC", "rpc", true)
-	plugins.AddPlugin(core.SCRIPT, "Evo Tree", "evotree", true)
+	plugins.AddPlugin(core.SCRIPT, "DiscordRPC", "rpc", true)
+	plugins.AddPlugin(core.SCRIPT, "Evotree", "evotree", true)
 	plugins.LoadConfig()
 	flags := plugins.InitPlugins()
 
 	ui, err := lorca.New("data:text/html,"+url.PathEscape(`
-		<title>Deeeep.io Desktop App</title>
+		<title>Deeeep.io Desktop Client</title>
+		<head>
+		<script src="https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.5/dist/html2canvas.min.js">
+		document.onkeyup = function(e) {
+			if (e.ctrlKey && e.which == 68) {
+				html2canvas(document.body).then((canvas) => {
+					let a = document.createElement("a");
+					a.download = "screenshot.png";
+					a.href = canvas.toDataURL("image/png");
+					// a.click();
+				  });
+			}
+		}
+		</script>
+		</head>
 		<style>*{padding: 0; margin: 0; overflow: hidden}
 		#loading{text-align: center; font-size: 40px; font-weight: bold; position: absolute; z-index: 2; color: white;}</style>
 		<!-- <img id="img" src="https://tinyurl.com/m5a4d3vw"></img> -->
@@ -39,10 +53,10 @@ func main() {
 	time.Sleep(2 * time.Second)
 	CheckAndLogFatal(err)
 
-	core.DiscordRpc(&ui)
-	ui.Bind("setConfig", func(config map[string]core.Config) {
+	core.DiscordRpc() //&ui)
+	/* ui.Bind("setConfig", func(config map[string]core.Config) {
 		plugins.Config = config
-	})
+	}) */
 
 	ui.Bind("reload", func() {
 		ui.Load(`https://beta.deeeep.io` + plugins.QueryPlugins())
@@ -51,7 +65,7 @@ func main() {
 	})
 
 	ui.Load(`https://beta.deeeep.io` + plugins.QueryPlugins())
-	ui.SetBounds(lorca.Bounds{0, 0, 1200, 1000, "maximized"})
+	ui.SetBounds(lorca.Bounds{0, 0, 1200, 800, ""})
 	plugins.ReloadPlugins(&ui)
 	EvalDefaultScripts(&ui, plugins)
 
@@ -59,6 +73,7 @@ func main() {
 		ui.Close()
 		plugins.SaveConfig()
 	}()
+
 	<-ui.Done()
 }
 
@@ -67,15 +82,15 @@ func EvalDefaultScripts(ref *lorca.UI, plugins core.PluginManager) {
 	config, _ := json.Marshal(plugins.Config)
 	data, _ := json.Marshal(plugins.Plugins)
 	ui.Eval(
-		"const config = " + string(config) + 
-		";const data = " + string(data),
+		"const config = " + string(config) +
+			";const data = " + string(data),
 	)
 	ui.Eval(`window.addEventListener("load", () => {
 		const app = document.getElementById("app")
 		const modal = document.createElement("div")
 		app.appendChild(modal)
-		modal.outerHTML =`  + "`" + string(index) + "`" +
-	`})`)
+		modal.outerHTML =` + "`" + string(index) + "`" +
+		`})`)
 	ui.Eval(string(script))
 }
 
