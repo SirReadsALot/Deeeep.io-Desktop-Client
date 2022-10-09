56ac109bf2e6f5cf59fcf7542c89c738c60a4555
SirReadsALot <72465167+SirReadsALot@users.noreply.github.com>
2022-10-09 13:21:16 +0400

    Game name storage, animal data logging and you can open evotree in-game

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/url"
 	"os"
-	"time"
 
 	core "github.com/SirReadsALot/deep_desktop/src"
 	"github.com/zserge/lorca"
@@ -17,6 +16,10 @@ import (
  */
 
 var script, _ = os.ReadFile("./src/script.js")
+
+var game_name, _ = os.ReadFile("./src/game_name.js")
+
+// var sw, _ = os.ReadFile("./src/swap.js")
 var index, _ = os.ReadFile("./src/index.html")
 
 func main() {
@@ -30,27 +33,16 @@ func main() {
 
 	ui, err := lorca.New("data:text/html,"+url.PathEscape(`
 		<title>Deeeep.io Desktop Client</title>
-		<head>
-		<script src="https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.5/dist/html2canvas.min.js">
-		document.onkeyup = function(e) {
-			if (e.ctrlKey && e.which == 68) {
-				html2canvas(document.body).then((canvas) => {
-					let a = document.createElement("a");
-					a.download = "screenshot.png";
-					a.href = canvas.toDataURL("image/png");
-					// a.click();
-				  });
-			}
-		}
-		</script>
-		</head>
-		<style>*{padding: 0; margin: 0; overflow: hidden}
-		#loading{text-align: center; font-size: 40px; font-weight: bold; position: absolute; z-index: 2; color: white;}</style>
+		<script>
+		<style>
+		*{padding: 0; margin: 0; overflow: hidden}
+		#loading{text-align: center; font-size: 40px; font-weight: bold; position:relative; z-index: 2; color: white;}
+		</style>
 		<!-- <img id="img" src="https://tinyurl.com/m5a4d3vw"></img> -->
+		<!-- <h3 id="loading">Loading...</h3> -->
 		<img id="img" src="https://sralcodeproj.netlify.app/assets/myhailot.png"></img>
-		<h3 id="loading">Loading...</h3>
 	`), "", 1120, 740, flags, "--disable-dinosaur-easter-egg") // previous: 887x586
-	time.Sleep(2 * time.Second)
+	// time.Sleep(2 * time.Second)
 	CheckAndLogFatal(err)
 
 	core.DiscordRpc() //&ui)
@@ -59,12 +51,57 @@ func main() {
 	}) */
 
 	ui.Bind("reload", func() {
+		ui.Close()
 		ui.Load(`https://beta.deeeep.io` + plugins.QueryPlugins())
 		plugins.ReloadPlugins(&ui)
 		EvalDefaultScripts(&ui, plugins)
 	})
 
-	ui.Load(`https://beta.deeeep.io` + plugins.QueryPlugins())
+	ui.Load(`https://deeeep.io` + plugins.QueryPlugins())
+	ui.Eval(string(game_name))
+	ui.Eval(`
+	  (function () {
+			var blockContextMenu
+	
+			blockContextMenu = function (evt) {
+			  evt.preventDefault();
+			  console.log("[DDC] [!] contextmenu blocked")
+			};
+	
+			window.addEventListener('contextmenu', blockContextMenu);
+		})();
+	
+		window.addEventListener('keydown', e => {
+			const evoModal = document.getElementById("evo-modal")
+			if (e.key == "q" && e.isComposing) {
+			  evoModal.classList.toggle("active")
+			  evoModal.transform = "scale(1)"
+			  evoModal.opacity = 1
+			  //alert("key Q held")
+			} else if (e.key != "q" || e.repeat) {
+				return;
+			} else {
+			  evoModal.classList.toggle("hidden")
+			  //alert("no key held")
+			}
+		}) 
+		window.addEventListener('keyup', e => {
+			if (e.key.toLowerCase() == "q") {
+				evoModal.style.transform = "scale (0)"
+				evoModal.style.opacity = 0
+			}
+		})
+	
+		console.log("[DDC] initiating fishLevelData logging function...")
+		window.addEventListener("keydown", e => {
+			if (e.key === "w") {
+				console.log(game.currentScene.myAnimal.fishLevelData)
+			} else {
+				return;
+			}
+		})
+	`)
+	// ui.Eval(string(sw))
 	ui.SetBounds(lorca.Bounds{0, 0, 1200, 800, ""})
 	plugins.ReloadPlugins(&ui)
 	EvalDefaultScripts(&ui, plugins)
