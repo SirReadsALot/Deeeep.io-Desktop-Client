51c51c6aba9b155e4b392e34e3f59e21ed9ed55e
SirReadsALot <72465167+SirReadsALot@users.noreply.github.com>
2023-04-29 16:42:45 +0400

    Add files via upload

--- plugins/docassets/background.js
+++ plugins/docassets/background.js
@@ -50,15 +50,23 @@ function toggleRedirect() {
 
 chrome.browserAction.onClicked.addListener(toggleRedirect); 
 
-//assets
-var bkgr = "https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/default/bgimage%20(Docassets).png";
-
-var clam1 = "https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/default/Pearl%20Shell%20display%20(Docassets).png";
-var clam2 = "https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/default/Pearl%20Stand%20display%20(Docassets).png";
-
 //script
 
 script = 'https://the-doctorpus.github.io/doc-assets/scripts/bundle.js';
+
+const alreadyChecked = new Set();
+
+function tempMarkChecked(toAdd) {
+    alreadyChecked.add(toAdd);
+
+    console.log(`${toAdd} temp-added to checked list`); 
+
+    setTimeout(function(toRemove) {
+        alreadyChecked.delete(toRemove);
+
+        console.log(`${toRemove} removed from checked list`); 
+    }, 5000, toAdd);
+}
   
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
@@ -75,51 +83,43 @@ chrome.webRequest.onBeforeRequest.addListener(
     ["blocking"]
 );
 
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        var assetUrl = options.redirectAssets ? bkgr : details.url; 
-        
-        return {redirectUrl: assetUrl};
-    },
-    {
-        urls: [
-            "https://deeeep.io/assets/bgimage.png?*"
-        ],
-        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
-    },
-    ["blocking"]
-);
-
-function genericHandler(redirectTemplate, regex, name) {
+function genericHandler(redirectTemplate, regex, name, filenameKeys=['filename']) {
     function handler(details) {
         let redirectUrl = details.url; 
-
+        
         if (options.redirectAssets) {
             const m = regex.exec(details.url); // checks if might be valid X
 
             console.log(`original ${name} URL is ${details.url}`); 
 
             if (m) {
-                const filename = m.groups.filename; 
+                const filenameArray = filenameKeys.map(key => m.groups[key] || '');
+                const filename = filenameArray.join('');
 
                 console.log(filename); 
 
                 let newRedirectUrl = redirectTemplate + filename; // redirect it
 
-                let checkRequest = new XMLHttpRequest(); // creates HTTP request
+                if (!alreadyChecked.has(newRedirectUrl)) {
+                    let checkRequest = new XMLHttpRequest(); // creates HTTP request
 
-                checkRequest.open('GET', newRedirectUrl, false); // sets up request
-                checkRequest.send(); // sends the request
+                    checkRequest.open('GET', newRedirectUrl, false); // sets up request
+                    checkRequest.send(); // sends the request
 
-                if (checkRequest.status >= 200 && checkRequest.status < 300) { // redirect exists
-                    redirectUrl = newRedirectUrl; 
+                    if (checkRequest.status >= 200 && checkRequest.status < 300) { // redirect exists
+                        redirectUrl = newRedirectUrl; 
 
-                    console.log(`Redirecting to ${newRedirectUrl}`); 
+                        console.log(`Redirecting to ${newRedirectUrl}`); 
+                    } else {
+                        tempMarkChecked(newRedirectUrl);
+
+                        console.log(`${newRedirectUrl} does not exist. Using default.`); 
+                    }
                 } else {
-                    console.log(`${newRedirectUrl} does not exist. Using default.`); 
+                    console.log(`Already checked ${newRedirectUrl}`); 
                 }
             } 
-        } 
+        }
 
         return  {
             redirectUrl: redirectUrl, 
@@ -129,9 +129,9 @@ function genericHandler(redirectTemplate, regex, name) {
     return handler; 
 }
 
-const ANIMATION_REDIRECT_TEMPLATE = 'https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/default/animations/'; // redirect URLs are all from this
+const ANIMATION_REDIRECT_TEMPLATE = 'https://the-doctorpus.github.io/doc-assets/images/default/animations/'; // redirect URLs are all from this
 const ANIMATION_SCHEME = '*://*.deeeep.io/assets/animations/*'; // these urls will be redirected like animations
-const ANIMATION_REGEX = /.+\/animations\/(?<filename>.+?)(?:\?.*)?$/ // might it be a valid animation? 
+const ANIMATION_REGEX = /.+\/animations\/(?<filename>[^?]+)(?:\?.*)?$/ // might it be a valid animation? 
 
 const animationHandler = genericHandler(ANIMATION_REDIRECT_TEMPLATE, ANIMATION_REGEX, 'animation'); 
 
@@ -146,9 +146,9 @@ chrome.webRequest.onBeforeRequest.addListener(
     ["blocking"]
 ); 
 
-const CHAR_REDIRECT_TEMPLATE = 'https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/characters/'; // redirect URLs are all from this
+const CHAR_REDIRECT_TEMPLATE = 'https://the-doctorpus.github.io/doc-assets/images/characters/'; // redirect URLs are all from this
 const CHAR_SCHEME = '*://*.deeeep.io/*assets/characters/*'; // these urls will be redirected like characters
-const CHAR_REGEX = /.+\/characters\/(?<filename>.+?)(?:\?.*)?$/ // might it be a valid character? 
+const CHAR_REGEX = /.+\/characters\/(?<filename>[^?]+)(?:\?.*)?$/ // might it be a valid character? 
 
 const charHandler = genericHandler(CHAR_REDIRECT_TEMPLATE, CHAR_REGEX, 'character'); 
 
@@ -163,9 +163,9 @@ chrome.webRequest.onBeforeRequest.addListener(
     ["blocking"]
 ); 
 
-const SPRITESHEET_REDIRECT_TEMPLATE = 'https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/default/spritesheets/'; // redirect URLs are all from this
+const SPRITESHEET_REDIRECT_TEMPLATE = 'https://the-doctorpus.github.io/doc-assets/images/default/spritesheets/'; // redirect URLs are all from this
 const SPRITESHEET_SCHEME = '*://*.deeeep.io/assets/spritesheets/*'; // these urls will be redirected like spritesheets
-const SPRITESHEET_REGEX = /.+\/spritesheets\/(?<filename>.+?)(?:\?.*)?$/ // might it be a valid spritesheet? 
+const SPRITESHEET_REGEX = /.+\/spritesheets\/(?<filename>[^?]+)(?:\?.*)?$/ // might it be a valid spritesheet? 
 
 const spritesheetHandler = genericHandler(SPRITESHEET_REDIRECT_TEMPLATE, SPRITESHEET_REGEX, 'spritesheet'); 
 
@@ -180,9 +180,9 @@ chrome.webRequest.onBeforeRequest.addListener(
     ["blocking"]
 ); 
 
-const MAP_SPRITESHEET_REDIRECT_TEMPLATE = 'https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/default/mapmaker-asset-packs/'; // redirect URLs are all from this
-const MAP_SPRITESHEET_SCHEME = '*://*.deeeep.io/mapmaker/assets/packs/*'; // these urls will be redirected like map spritesheets
-const MAP_SPRITESHEET_REGEX = /.+\/packs\/(?<filename>.+?)(?:\?.*)?$/ // might it be a valid map spritesheet? 
+const MAP_SPRITESHEET_REDIRECT_TEMPLATE = 'https://the-doctorpus.github.io/doc-assets/images/default/mapmaker-asset-packs/'; // redirect URLs are all from this
+const MAP_SPRITESHEET_SCHEME = '*://*.deeeep.io/assets/packs/*'; // these urls will be redirected like map spritesheets
+const MAP_SPRITESHEET_REGEX = /.+\/packs\/(?<filename>[^?]+)(?:\?.*)?$/ // might it be a valid map spritesheet? 
 
 const mapSpritesheetHandler = genericHandler(MAP_SPRITESHEET_REDIRECT_TEMPLATE, MAP_SPRITESHEET_REGEX, 'map spritesheet'); 
 
@@ -197,56 +197,56 @@ chrome.webRequest.onBeforeRequest.addListener(
     ["blocking"]
 ); 
 
-const SKIN_REDIRECT_TEMPLATE = 'https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/skans/'; // redirect URLs are all from this
-const SKIN_SCHEME = '*://*.deeeep.io/assets/skins/*'; // these urls will be redirected like skins
-const SKIN_REGEX = /.+\/skins\/(?<filename>.+?)(?:\?.*)?$/ // might it be a valid skin? 
-const CUSTOM_REGEX = /(?<pre_version>custom\/(?<skin_id>[0-9]+))(?<version>-[0-9]+)(?<post_version>(?<extra_asset_name>-[A-Za-z0-9-_]+)?\.(?<suffix>[a-zA-Z0-9]+))/
-// skins submitted through Creators Center have a special scheme and must be stripped of their version number
+const IMG_REDIRECT_TEMPLATE = 'https://the-doctorpus.github.io/doc-assets/images/img/'; // redirect URLs are all from this
+const IMG_SCHEME = '*://*.deeeep.io/img/*'; // these urls will be redirected like ui sprites
+const IMG_REGEX = /.+\/img\/(?<filename>[^?]+)(?:\?.*)?$/ // might it be a valid ui sprite? 
+
+const imgSpriteHandler = genericHandler(IMG_REDIRECT_TEMPLATE, IMG_REGEX, 'img spritesheet'); 
 
 chrome.webRequest.onBeforeRequest.addListener(
-    function skinHandler(details) {
-        let redirectUrl = details.url; 
-        
-        if (options.redirectAssets) {
-            const m = SKIN_REGEX.exec(details.url); // checks if might be valid skin
-
-            console.log(`original skin URL is ${details.url}`); 
-
-            if (m) {
-                let filename = m.groups.filename; 
-
-                const m2 = CUSTOM_REGEX.exec(filename); // checks if might be Creators Center skin
-
-                if (m2) {
-                    filename = m2.groups.pre_version + m2.groups.post_version; // ignoring the version number
-                } 
-                
-                console.log(filename); 
-
-                newRedirectUrl = SKIN_REDIRECT_TEMPLATE + filename; // builds redirect URL
-
-                let checkRequest = new XMLHttpRequest(); // creates HTTP request
-
-                checkRequest.open('GET', newRedirectUrl, false); // sets up request
-                checkRequest.send(); // sends the request
-
-                if (checkRequest.status >= 200 && checkRequest.status < 300) { // redirect exists
-                    redirectUrl = newRedirectUrl; 
-
-                    console.log(`Redirecting to ${newRedirectUrl}`); 
-                } else {
-                    console.log(`${newRedirectUrl} does not exist. Using default.`); 
-                }
-            } 
-        } 
-
-        return  {
-            redirectUrl: redirectUrl, 
-        }; 
-    },
+    imgSpriteHandler, 
     {
         urls: [
-            SKIN_SCHEME
+            IMG_SCHEME
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+); 
+
+const PET_REDIRECT_TEMPLATE = 'https://the-doctorpus.github.io/doc-assets/images/custom/pets/';
+const PET_SCHEME = '*://*.deeeep.io/custom/pets/*'
+const PET_REGEX = /.+\/pets\/(?<filename>[^?]+)(?:\?.*)?$/
+
+const petHandler = genericHandler(PET_REDIRECT_TEMPLATE, PET_REGEX, 'pet'); 
+
+chrome.webRequest.onBeforeRequest.addListener(
+    petHandler, 
+    {
+        urls: [
+            PET_SCHEME
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+); 
+
+const SKIN_REDIRECT_TEMPLATE = 'https://the-doctorpus.github.io/doc-assets/images/skans/'; // redirect URLs are all from this
+const CDN_SKIN_REDIRECT_TEMPLATE = 'https://the-doctorpus.github.io/doc-assets/images/skans/custom/'; // redirect URLs are all from this
+const SKIN_SCHEME = '*://*.deeeep.io/assets/skins/*'; // these urls will be redirected like skins
+const CDN_SKIN_SCHEME = '*://cdn.deeeep.io/custom/skins/*';
+const SKIN_REGEX = /.+\/skins\/(?<filename>[^?]+)(?:\?.*)?$/; // might it be a valid skin? 
+const CDN_REGEX = /skins\/(?:(?<skin_name>[A-Za-z]+)|(?:(?<skin_id>[0-9]+)(?<version>-[0-9]+)(?<post_version>(?<extra_asset_name>-[A-Za-z0-9-_]+)?)))(?<suffix>\.[a-zA-Z0-9]+)/;
+// skins submitted through Creators Center have a special scheme and must be stripped of their version number
+
+const nonCDNSkinHandler = genericHandler(SKIN_REDIRECT_TEMPLATE, SKIN_REGEX, 'non-CDN skin');
+const CDNSkinHandler = genericHandler(CDN_SKIN_REDIRECT_TEMPLATE, CDN_REGEX, 'CDN skin', ['skin_name', 'skin_id', 'post_version', 'suffix']);
+
+chrome.webRequest.onBeforeRequest.addListener(
+    nonCDNSkinHandler,
+    {
+        urls: [
+            SKIN_SCHEME,
         ],
         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     },
@@ -254,44 +254,29 @@ chrome.webRequest.onBeforeRequest.addListener(
 ); 
 
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        var assetUrl = options.redirectAssets ? clam1 : details.url; 
-
-        return {redirectUrl: assetUrl};
-    },
+    CDNSkinHandler,
     {
         urls: [
-            "https://deeeep.io/assets/pearl_cover.png"
+            CDN_SKIN_SCHEME,
         ],
         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     },
     ["blocking"]
 );
+
+const MISC_REDIRECT_TEMPLATE = 'https://the-doctorpus.github.io/doc-assets/images/misc/'; // redirect URLs are all from this
+const MISC_SCHEME = '*://*.deeeep.io/assets/*'; // these urls will be redirected like ui sprites
+const MISC_REGEX = /.+\/assets\/(?<filename>[^/?]+)(?:\?.*)?$/ // might it be a valid ui sprite? 
+
+const miscHandler = genericHandler(MISC_REDIRECT_TEMPLATE, MISC_REGEX, 'misc'); 
 
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        var assetUrl = options.redirectAssets ? clam2 : details.url; 
-
-        return {redirectUrl: assetUrl};
-    },
+    miscHandler, 
     {
         urls: [
-            "https://deeeep.io/assets/pearl_stand_only.png"
+            MISC_SCHEME
         ],
         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     },
     ["blocking"]
-);
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.url) {
-        const url = new URL(changeInfo.url);
-        if (url.searchParams.get("docassets") == "true") {
-            DEFAULTS.redirectAssets = true
-            options.redirectAssets = true
-        } else {
-            DEFAULTS.redirectAssets = false
-            options.redirectAssets = false
-        }
-    }
-});
+); 
